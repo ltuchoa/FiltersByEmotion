2b4e3312ba2b490e8774949c3c22734633c26de1
ltuchoa <ltuchoa@gmail.com>
2021-06-18 08:26:28 -0300

    Initial Commit

---
+++ FiltersByEmotion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FiltersByEmotion
+//
+//  Created by Larissa Uchoa on 18/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
